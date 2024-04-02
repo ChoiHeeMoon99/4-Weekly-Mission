8749bf26e2788d41429337bdfaddd744c027497c
gun7741 <gun7741@naver.com>
2024-04-03 04:35:18 +0900

    feat: api fetch

---
+++ src/apis/FetchData.tsx
@@ -0,0 +1,7 @@
+export async function fetchData(query: string): Promise<any> {
+  const result = await fetch(`https://bootcamp-api.codeit.kr/api/${query}`);
+  // const result = await fetch(`${process.env.REACT_APP_API_URL}${query}`);
+  const body = await result.json();
+  return body;
+}
+export default fetchData;
