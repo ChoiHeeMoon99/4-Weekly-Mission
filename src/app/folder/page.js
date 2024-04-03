f212b2eae37242f63cb62ee160b56714471bc68e
gun7741 <gun7741@naver.com>
2024-04-03 14:49:01 +0900

    chore : page에 header footer추가

--- src/app/folder/page.js
+++ src/app/folder/page.js
@@ -1,13 +1,17 @@
+import Footer from "@/src/components/Footer";
+import HeaderNavigation from "@/src/components/HeaderNavigation";
 import AddLink from "@/src/components/addLink";
 import FolderList from "@/src/components/folderList";
 
 export default function Folder() {
   return (
     <div>
+      <HeaderNavigation />
       <AddLink />
       <div style={{ display: "flex", justifyContent: "center" }}>
         <FolderList />
       </div>
+      <Footer />
     </div>
   );
 }
--- src/app/shared/page.js
+++ src/app/shared/page.js
@@ -1,12 +1,16 @@
 import FolderComponent from "@/src/components/folderComponent";
 import Card from "@/src/components/Card";
+import HeaderNavigation from "@/src/components/HeaderNavigation";
+import Footer from "@/src/components/Footer";
 export default function Shared() {
   return (
     <div>
+      <HeaderNavigation />
       <FolderComponent />
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Card />
       </div>
+      <Footer />
     </div>
   );
 }
